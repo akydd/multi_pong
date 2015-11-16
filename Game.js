5d6163fa71c1099426f153832a886bebaac1b863
Alan Kydd <akydd@ualberta.net>
2015-11-15 21:43:50 -0700

    Added movement for one paddle

--- Game.js
+++ Game.js
@@ -7,17 +7,26 @@ Game.Game.prototype = {
         this.physics.arcade.checkCollision.down = false;
         this.physics.arcade.checkCollision.up = false;
 
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         this.createPaddles();
         this.createScoreBoard();
     },
     update: function() {
+        // paddle motion
+        this.player1.body.velocity.x = 0;
+        if (this.cursors.left.isDown) {
+            this.player1.body.velocity.x = -600;
+        } else if (this.cursors.right.isDown) {
+            this.player1.body.velocity.x = 600;
+        }
     },
     createPaddles: function() {
-        var player1 = new Paddle(this, 0, 0);
-        var player2 = new Paddle(this, 200, 200);
+        this.player1 = new Paddle(this, 320, 40);
+        this.player2 = new Paddle(this, 320, 920);
 
-        this.add.existing(player1);
-        this.add.existing(player2);
+        this.add.existing(this.player1);
+        this.add.existing(this.player2);
     },
     createScoreBoard: function() {
     }
--- Paddle.js
+++ Paddle.js
@@ -3,10 +3,11 @@
 var Paddle = function(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'loadingBar');
 
-//    this.anchor.setTo(0.5);
-//
-//    game.physics.arcade.enableBody(this);
-//    this.checkWorldBounds = true;
+    this.anchor.setTo(0.5);
+
+    game.physics.arcade.enableBody(this);
+    this.checkWorldBounds = true;
+    this.body.collideWorldBounds = true;
 };
 
 Paddle.prototype = Object.create(Phaser.Sprite.prototype);
