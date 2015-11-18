da5401cab94cf52272960ed20b40f4ea9288b175
Alan Kydd <akydd@ualberta.net>
2015-11-18 13:25:31 -0700

    Basic socket.io connections established

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express')
   , app = express(app)
-  , server = require('http').createServer(app);
+  , server = require('http').createServer(app)
+  , io = require('socket.io')(server);
 
 
 // serve static files from the current directory
 app.use(express.static(__dirname));
 
 server.listen(8000);
+
+io.on('connection', function(client) {
+  console.log('Client connected...');
+
+  client.on('join', function(data) {
+    console.log(data);
+  });
+});
