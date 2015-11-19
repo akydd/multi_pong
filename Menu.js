e70ec539834eb6f35d18cd3849331688c169a977
Alan Kydd <akydd@ualberta.net>
2015-11-18 20:32:05 -0700

    Working on starting game when two connected clients indicate readiness.

--- Menu.js
+++ Menu.js
@@ -4,14 +4,22 @@ Game.Menu = function(game) {
 Game.Menu.prototype = {
     create: function() {
         var textStyle = {fill: '#ffffff'};
-        var titleText = this.add.text(320, 480 - 40, "Up to start", textStyle);
-        titleText.anchor.setTo(0.5, 0.5);
+        this.titleText = this.add.text(320, 480 - 40, "Press Up arrow when ready", textStyle);
+        this.titleText.anchor.setTo(0.5, 0.5);
 
         this.cursor = this.input.keyboard.createCursorKeys();
     },
     update: function() {
+        var socket = io();
+
+        var _this = this;
+        socket.on('startgame', function() {
+            _this.state.start('Game');
+        });
+
         if (this.cursor.up.isDown) {
-            this.state.start('Game');
+            this.titleText.text = "Waiting for other player";
+            socket.emit('playerReady');
         }
     }
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express')
   , app = express(app)
   , server = require('http').createServer(app)
-  , io = require('socket.io')(server);
+  , io = require('socket.io')(server)
+  , _ = require('underscore');
 
 
 // serve static files from the current directory
@@ -9,10 +10,40 @@ app.use(express.static(__dirname));
 
 server.listen(8000);
 
+var playersState = [];
+
+// Handle socket connection
 io.on('connection', function(client) {
   console.log('Client connected...');
+  console.log(io.sockets.sockets.length + " connections");
 
-  client.on('join', function(data) {
-    console.log(data);
+  playersState.push(
+    {
+      id: client.id,
+      ready: false
+    }
+  );
+
+  client.on('playerReady', function() {
+    markPlayerAsReady(client.id);
+
+    if (allPlayersAreReady()) {
+      io.emit('startgame');
+    }
   });
+
+  client.on('disconnect', function() {
+    console.log('Client disconnected');
+    console.log(io.sockets.sockets.length + " connections");
+  });
+
 });
+
+function markPlayerAsReady(clientId) {
+  // TODO
+}
+
+function allPlayersAreReady() {
+  // TODO
+  return false;
+}
