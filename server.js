e65361b6b5cc03ee3806ca003743365b62139123
Alan Kydd <akydd@ualberta.net>
2015-11-18 22:58:22 -0700

    Working on starting game when two connected clients indicate readiness.

--- server.js
+++ server.js
@@ -23,27 +23,39 @@ io.on('connection', function(client) {
       ready: false
     }
   );
+  console.log(playersState);
 
   client.on('playerReady', function() {
     markPlayerAsReady(client.id);
 
-    if (allPlayersAreReady()) {
+    if (allPlayersAreReady() === true) {
       io.emit('startgame');
     }
   });
 
   client.on('disconnect', function() {
     console.log('Client disconnected');
+    var clientIndex = _.findIndex(playersState, {id: client.id});
+    if (clientIndex > -1) {
+      playersState.splice(clientIndex, 1);
+    }
     console.log(io.sockets.sockets.length + " connections");
   });
 
 });
 
 function markPlayerAsReady(clientId) {
-  // TODO
+  _.each(playersState, function(playerState) {
+    if (playerState.id === clientId) {
+      playerState.ready = true;
+    }
+  });
+  console.log(playersState);
 }
 
 function allPlayersAreReady() {
-  // TODO
-  return false;
+  var readyPlayers = _.where(playersState, {ready: true});
+  console.log(readyPlayers.length);
+  console.log(playersState.length);
+  return (readyPlayers.length === playersState.length);
 }
